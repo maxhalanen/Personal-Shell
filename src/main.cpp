8b04354bd38e98ada0feb2aae9fa62a594211341
Max Halanen <maximilian.halanen@gmail.com>
2024-06-14 22:00:14 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,14 @@ int main() {
     else if(input.compare(0, 4, "echo") == 0) {
       std::cout << input.substr(5) << std::endl;
     }
+    else if(input.compare(0, 4, "type") == 0) {
+      if(input.substr(5) == "echo" || input.substr(5) == "type" || input.substr(5) == "exit") {
+        std::cout << input.substr(5) << " is a shell builtin" << std::endl;
+      }
+      else{
+         std::cout << input.substr(5) << ": not found" << std::endl;
+      }
+    }
     else{
       std::cout << input << ": command not found" << std::endl;
     }
